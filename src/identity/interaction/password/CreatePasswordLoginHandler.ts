67630ba8c0e2083c161200dba34c46ad50ecd9e6
thhck <thhck@proton.me>
2025-03-27 01:53:16 +0100

    fix a few bugs, better endpoints, readme

--- src/identity/interaction/password/CreatePasswordLoginHandler.ts
+++ src/identity/interaction/password/CreatePasswordLoginHandler.ts
@@ -19,6 +19,10 @@ import {
   CookieStore,
   EmptyObject,
   LoginOutputType,
+  PodStore,
+  PodCreator,
+  IdentifierGenerator,
+  BadRequestHttpError,
 } from '@solid/community-server';
 
 interface InputData {
@@ -36,63 +40,62 @@ const inSchema = object({
 });
 
 export interface CreatePasswordLoginHandlerArgs {
-  createAccountHandler: CreateAccountHandler;
-  createPodHandler: CreatePodHandler;
   accountStore: AccountStore;
+  cookieStore: CookieStore;
   passwordStore: PasswordStore;
   passwordRoute: PasswordIdRoute;
-  cookieStore: CookieStore;
+  podCreator: PodCreator;
+  podStore: PodStore;
+  identifierGenerator: IdentifierGenerator;
 }
 
 type OutType = {
   resource: string;
 }
 export class CreatePasswordLoginHandler extends ResolveLoginHandler implements JsonView {
-  private readonly createAccountHandler: CreateAccountHandler;
-  private readonly createPodHandler: CreatePodHandler;
-  // private readonly accountStore: AccountStore;
   private readonly passwordStore: PasswordStore;
   private readonly passwordRoute: PasswordIdRoute;
+  private readonly podCreator: PodCreator;
+  private readonly podStore: PodStore;
+  private readonly identifierGenerator: IdentifierGenerator;
   public constructor(args: CreatePasswordLoginHandlerArgs) {
     super(args.accountStore, args.cookieStore);
-    this.createAccountHandler = args.createAccountHandler;
-    this.createPodHandler = args.createPodHandler;
-    // this.accountStore = args.accountStore;
     this.passwordStore = args.passwordStore;
     this.passwordRoute = args.passwordRoute;
+    this.podCreator = args.podCreator;
+    this.podStore = args.podStore;
+    this.identifierGenerator = args.identifierGenerator;
   }
 
   public async getView(): Promise<JsonRepresentation<EmptyObject>> {
-    return { json: {}};
+    return { json: {} };
   }
 
-  public async login({json}: JsonInteractionHandlerInput): Promise<JsonRepresentation<LoginOutputType>> {
+  public async login(input: JsonInteractionHandlerInput): Promise<JsonRepresentation<LoginOutputType>> {
+    const { json } = input;
     const accountId = await this.accountStore.create();
- // const accountId = await this.accountStore.create();
 
-    const validatedData = await validateWithError(inSchema, json) as InputData;
-    // Create password login for the account
+    // validate the input
+    const { email, password, name } = await validateWithError(inSchema, json);
+    // we need to check if the pod already exists, so if first
+    // because otherwise we would have to delete the created account
+    // if the pod creation fails
+    const podId = await this.identifierGenerator.generate(name);
+    let res = await this.podStore.findByBaseUrl(podId.path)
+    if (res) throw new BadRequestHttpError('Pod already exists');
 
-    const { email, password } = await validateWithError(inSchema, json);
-    const passwordId = await this.passwordStore.create(email, accountId, password);
+  const passwordId = await this.passwordStore.create(email, accountId, password);
     const resource = this.passwordRoute.getPath({ accountId, passwordId });
 
     // If we ever want to add email verification this would have to be checked separately
     await this.passwordStore.confirmVerification(passwordId);
 
-    // return { json: { resource } };
-    // Create new pod for the account
-    const podResponse = await this.createPodHandler.handle({
+
+    await this.podCreator.handleSafe({
       accountId,
-      json: {
-        name: validatedData.name,
-      },
-    } as JsonInteractionHandlerInput);
+      name,
+    });
 
-    // TODO: Need to get the account URL from the server
-    const accountUrl = `http://localhost:3000/.account/account/${accountId}`;
-
-
-    return { json: { accountId }};
+    return { json: { accountId } };
   }
 }
